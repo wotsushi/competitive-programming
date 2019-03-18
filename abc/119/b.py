83533f7e474a83f7d7813ca0613a267ea5f0c3ba
wotsushi <wotsushi@gmail.com>
2019-03-18 22:09:02 +0900

    Solve ABC119-B by Python

---
+++ abc/119/b.py
@@ -0,0 +1,12 @@
+# 入力
+N = int(input())
+x, u = zip(*((float(t[0]), t[1]) for t in (input().split() for _ in range(N))))
+
+# 定義通り計算
+ans = sum(
+    t[0] if t[1] == 'JPY' else
+    380000 * t[0] for t in zip(x, u)
+)
+
+# 出力
+print(ans)
