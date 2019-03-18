2a0d171963f4ab0f1821db20dc8ed379e9d8542d
wotsushi <wotsushi@gmail.com>
2019-03-18 22:13:22 +0900

    Fix BR

--- abc/119/b.py
+++ abc/119/b.py
@@ -5,7 +5,8 @@ x, u = zip(*((float(t[0]), t[1]) for t in (input().split() for _ in range(N))))
 # 定義通り計算
 ans = sum(
     t[0] if t[1] == 'JPY' else
-    380000 * t[0] for t in zip(x, u)
+    380000 * t[0]
+    for t in zip(x, u)
 )
 
 # 出力
