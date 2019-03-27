d52e78c1c5a1e84d842ae9c4afb716b136e54a1a
wotsushi <wotsushi@gmail.com>
2019-03-27 23:36:15 +0900

    Add comment into ABC106-D

--- abc/106/d.py
+++ abc/106/d.py
@@ -3,13 +3,16 @@ from functools import lru_cache
 import sys
 sys.setrecursionlimit(2*10**5)
 
+# 入力
 N, M, Q = map(int, input().split())
 L, R = zip(*(map(int, input().split()) for _ in range(M)))
 p, q = zip(*(map(int, input().split()) for _ in range(Q)))
 
+# 各区間を走る列車の本数
 c = Counter(zip(L, R))
 
 
+# 走る区間が区間 [x, y] に含まれる列車の本数
 @lru_cache(maxsize=None)
 def f(x, y):
     return (
@@ -18,5 +21,8 @@ def f(x, y):
     )
 
 
+# 各クエリに対してfを用いて回答
 ans = '\n'.join(map(str, (f(x, y) for x, y in zip(p, q))))
+
+# 出力
 print(ans)
