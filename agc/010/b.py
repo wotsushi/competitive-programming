133777b09eb25fcb047f330964403b7284c0b94f
wotsushi <wotsushi@gmail.com>
2019-04-03 22:41:35 +0900

    Solve AGC010-B by Python

---
+++ agc/010/b.py
@@ -0,0 +1,20 @@
+# 入力
+N = int(input())
+A = list(map(int, input().split()))
+
+S = sum(A)
+
+# A_{i + 1} - A_i から解を求める
+ans = (
+    'YES' if (2 * S) % (N * (N + 1)) == 0 and all(
+        k % N == 0 and k >= 0
+        for k in (
+            (2 * S) // (N * (N + 1)) - (A[i] - A[i - 1])
+            for i in range(N)
+        )
+    ) else
+    'NO'
+)
+
+# 出力
+print(ans)
