4ed7fcdf99c387fced1f4a63c3a6b26e52859c01
wotsushi <wotsushi@gmail.com>
2019-04-24 20:40:51 +0900

    Solve AGC003-B by Python

---
+++ agc/003/b.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+# 入力
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+# 貪欲法に基づき、小さい整数から順にペアを作成
+ans = reduce(
+    lambda acc, a: (acc[0] + (a + acc[1]) // 2, max(0, a - acc[1]) % 2),
+    A,
+    (0, 0)
+)[0]
+
+# 出力
+print(ans)
