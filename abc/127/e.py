7653038557e51c37f7a1219bd2813e37771cdf20
wotsushi <wotsushi@gmail.com>
2019-05-26 17:38:53 +0900

    Solve ABC127-E by Python

---
+++ abc/127/e.py
@@ -0,0 +1,81 @@
+from functools import reduce
+
+# 入力
+N, M, K = map(int, input().split())
+
+MOD = 10**9 + 7
+
+
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x - self.x) if isinstance(other, ModInt) else
+            ModInt(other - self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, MOD - 2, MOD))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, MOD))
+        )
+
+
+# (N * M * (M * (N**2 - 1) + N * (M**2 - 1)) // 6) * (NM - 2)C(K - 2) が解
+ans = (N * M * (M * (N**2 - 1) + N * (M**2 - 1)) // 6) * reduce(
+    lambda acc, i: acc * (N * M - 2 - i) / (K - 2 - i),
+    range(K - 2),
+    ModInt(1)
+)
+print(ans)
