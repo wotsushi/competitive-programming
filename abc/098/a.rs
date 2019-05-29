dc7867da6443b2d6ea65571852756c698c4d433c
wotsushi <wotsushi@gmail.com>
2019-05-29 23:04:18 +0900

    Solve ABC098-A by Rust

---
+++ abc/098/a.rs
@@ -0,0 +1,51 @@
+macro_rules! get {
+      ($t:ty) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.trim().parse::<$t>().unwrap()
+          }
+      };
+      ($($t:ty),*) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              let mut iter = line.split_whitespace();
+              (
+                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+              )
+          }
+      };
+      ($t:ty; $n:expr) => {
+          (0..$n).map(|_|
+              get!($t)
+          ).collect::<Vec<_>>()
+      };
+      ($($t:ty),*; $n:expr) => {
+          (0..$n).map(|_|
+              get!($($t),*)
+          ).collect::<Vec<_>>()
+      };
+      ($t:ty ;;) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.split_whitespace()
+                  .map(|t| t.parse::<$t>().unwrap())
+                  .collect::<Vec<_>>()
+          }
+      };
+      ($t:ty ;; $n:expr) => {
+          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+      };
+}
+
+fn main() {
+    // 入力
+    let (A, B) = get!(i64, i64);
+
+    let ans = vec![A + B, A - B, A * B].into_iter().max().unwrap();
+
+    // 出力
+    println!("{}", ans);
+}
