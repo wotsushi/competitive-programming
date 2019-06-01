813fd66500bb09ee19492770725ecc329fc9e04c
wotsushi <wotsushi@gmail.com>
2019-06-01 11:28:24 +0900

    Solve ABC116-B by Rust

---
+++ abc/116/b.rs
@@ -0,0 +1,65 @@
+macro_rules! get {
+      ($t:ty) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.trim().parse::<$t>().unwrap()
+          }
+      };
+      ($($t:ty),*) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              let mut iter = line.split_whitespace();
+              (
+                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+              )
+          }
+      };
+      ($t:ty; $n:expr) => {
+          (0..$n).map(|_|
+              get!($t)
+          ).collect::<Vec<_>>()
+      };
+      ($($t:ty),*; $n:expr) => {
+          (0..$n).map(|_|
+              get!($($t),*)
+          ).collect::<Vec<_>>()
+      };
+      ($t:ty ;;) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.split_whitespace()
+                  .map(|t| t.parse::<$t>().unwrap())
+                  .collect::<Vec<_>>()
+          }
+      };
+      ($t:ty ;; $n:expr) => {
+          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+      };
+}
+
+use std::collections::HashSet;
+
+fn main() {
+    // 入力
+    let s = get!(i64);
+
+    let mut a = s;
+    let mut m = 1;
+    let mut dp = HashSet::new();
+    while !dp.contains(&a) {
+        dp.insert(a);
+        a = if a % 2 == 0 {
+            a / 2
+        } else {
+            3 * a + 1
+        };
+        m += 1;
+    }
+    let ans = m;
+
+    // 出力
+    println!("{}", ans);
+}
