4b20b491cdf2bb57895922de6ce8968bfa81d369
wotsushi <wotsushi@gmail.com>
2019-06-08 00:09:12 +0900

    Solve ABC083-A by Rust

---
+++ abc/083/a.rs
@@ -0,0 +1,57 @@
+macro_rules! get {
+      ($t:ty) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.trim().parse::<$t>().unwrap()
+          }
+      };
+      ($($t:ty),*) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              let mut iter = line.split_whitespace();
+              (
+                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+              )
+          }
+      };
+      ($t:ty; $n:expr) => {
+          (0..$n).map(|_|
+              get!($t)
+          ).collect::<Vec<_>>()
+      };
+      ($($t:ty),*; $n:expr) => {
+          (0..$n).map(|_|
+              get!($($t),*)
+          ).collect::<Vec<_>>()
+      };
+      ($t:ty ;;) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.split_whitespace()
+                  .map(|t| t.parse::<$t>().unwrap())
+                  .collect::<Vec<_>>()
+          }
+      };
+      ($t:ty ;; $n:expr) => {
+          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+      };
+}
+
+fn main() {
+    // 入力
+    let (A, B, C, D) = get!(i64, i64, i64, i64);
+
+    let ans = if A + B > C + D {
+        "Left"
+    } else if A + B == C + D {
+        "Balanced"
+    } else {
+        "Right"
+    };
+
+    // 出力
+    println!("{}", ans);
+}
