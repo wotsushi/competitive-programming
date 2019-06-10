343ce65c8f3504a7605971172d882a05ce163381
wotsushi <wotsushi@gmail.com>
2019-06-10 12:37:13 +0900

    Solve ABC129-E by Python

---
+++ abc/129/e.py
@@ -0,0 +1,80 @@
+from functools import reduce
+
+# 入力
+L = input()
+
+MOD = 10**9 + 7
+
+
+# 最上位ビットから順に、0にする場合と1にする場合の個数を求める
+# 最上位ビットが0の場合、直ちに解が求められる（3のべき乗になる）ことに注意
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(
+                pow(self.x, other.x, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    def __radd__(self, other):
+        return ModInt(other + self.x)
+
+    def __rsub__(self, other):
+        return ModInt(other - self.x)
+
+    def __rmul__(self, other):
+        return ModInt(other * self.x)
+
+    def __rtruediv__(self, other):
+        return ModInt(other * pow(self.x, MOD - 2, MOD))
+
+    def __rpow__(self, other):
+        return ModInt(pow(other, self.x, MOD))
+
+
+ans = reduce(
+    lambda acc, t: (
+        ModInt(3)**t[0] + 2 * acc if t[1] == '1' else
+        acc
+    ),
+    enumerate(reversed(L)),
+    ModInt(1)
+)
+
+# 出力
+print(ans)
