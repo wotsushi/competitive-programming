6930b25e86117bb0ea56ad489cec3dbc3fc60eba
wotsushi <wotsushi@gmail.com>
2019-06-12 00:18:40 +0900

    Solve Tenka1-2017-B by Rust

---
+++ etc/tenka1-2017/README.md
@@ -0,0 +1,7 @@
+# Tenka1 Programmer Beginner Contest
+
+## B
+カテゴリ: 数学
+
+i = argmax A とする。
+解は A_i + B_i である。
---
+++ etc/tenka1-2017/b.rs
@@ -0,0 +1,56 @@
+macro_rules! get {
+      ($t:ty) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.trim().parse::<$t>().unwrap()
+          }
+      };
+      ($($t:ty),*) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              let mut iter = line.split_whitespace();
+              (
+                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+              )
+          }
+      };
+      ($t:ty; $n:expr) => {
+          (0..$n).map(|_|
+              get!($t)
+          ).collect::<Vec<_>>()
+      };
+      ($($t:ty),*; $n:expr) => {
+          (0..$n).map(|_|
+              get!($($t),*)
+          ).collect::<Vec<_>>()
+      };
+      ($t:ty ;;) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.split_whitespace()
+                  .map(|t| t.parse::<$t>().unwrap())
+                  .collect::<Vec<_>>()
+          }
+      };
+      ($t:ty ;; $n:expr) => {
+          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+      };
+}
+
+fn main() {
+    // 入力
+    let N = get!(i64);
+    let (A, B): (Vec<_>, Vec<_>) = get!(i64, i64; N).into_iter().unzip();
+
+    let mut AB = A.iter()
+        .zip(B.iter())
+        .collect::<Vec<_>>();
+    AB.sort_by_key(|&(a, _)| -a);
+    let (a, b) = AB[0];
+    let ans = a + b;
+    // 出力
+    println!("{}", ans);
+}
