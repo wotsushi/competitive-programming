939bcd0ba328f5d122199e9202de673d2d47cacb
wotsushi <wotsushi@gmail.com>
2019-06-12 23:32:07 +0900

    Solve ABC062-B by Rust

---
+++ abc/062/b.rs
@@ -0,0 +1,60 @@
+macro_rules! get {
+      ($t:ty) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.trim().parse::<$t>().unwrap()
+          }
+      };
+      ($($t:ty),*) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              let mut iter = line.split_whitespace();
+              (
+                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+              )
+          }
+      };
+      ($t:ty; $n:expr) => {
+          (0..$n).map(|_|
+              get!($t)
+          ).collect::<Vec<_>>()
+      };
+      ($($t:ty),*; $n:expr) => {
+          (0..$n).map(|_|
+              get!($($t),*)
+          ).collect::<Vec<_>>()
+      };
+      ($t:ty ;;) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.split_whitespace()
+                  .map(|t| t.parse::<$t>().unwrap())
+                  .collect::<Vec<_>>()
+          }
+      };
+      ($t:ty ;; $n:expr) => {
+          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+      };
+}
+
+fn main() {
+    // 入力
+    let (H, W) = get!(i64, i64);
+    let a = get!(String; H);
+
+    let x = (0..(W + 2)).map(|_| "#")
+        .collect::<Vec<_>>()
+        .join("");
+    let ans = [
+        &x[..],
+        &a.into_iter().map(|b| format!("#{}#", b))
+            .collect::<Vec<_>>()
+            .join("\n"),
+        &x[..]
+    ].join("\n");
+    // 出力
+    println!("{}", ans);
+}
