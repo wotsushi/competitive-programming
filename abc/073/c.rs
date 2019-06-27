6634b1e854cfb1f4c0842877c2fee64dbf8533d1
wotsushi <wotsushi@gmail.com>
2019-06-28 02:23:12 +0900

    Solve ABC073-C by Rust

---
+++ abc/073/c.rs
@@ -0,0 +1,61 @@
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+use std::collections::HashSet;
+
+fn main() {
+    let N = get!(i64);
+    let A = get!(i64; N);
+
+    // Setで各数字を管理する
+    let mut S = HashSet::new();
+    for a in A {
+        if S.contains(&a) {
+            S.remove(&a);
+        } else {
+            S.insert(a);
+        }
+    }
+    let ans = S.len();
+
+    println!("{}", ans);
+}
