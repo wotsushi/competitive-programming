eba5aa41d8d7a974e8e88f90fc623edc648292e3
wotsushi <wotsushi@gmail.com>
2019-06-30 15:27:09 +0900

    Solve ABC132-A by Rust

---
+++ abc/132/a.rs
@@ -0,0 +1,55 @@
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let S = get!(String);
+
+    let mut A = S.chars().collect::<Vec<_>>();
+    A.sort();
+    let ans = if A[0] == A[1] && A[1] != A[2] && A[2] == A[3] {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
+}
