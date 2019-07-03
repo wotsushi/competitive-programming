11007c3f58abb30d12dc84d2beac1bd79c6b079a
wotsushi <wotsushi@gmail.com>
2019-07-03 16:05:45 +0900

    Solve ARC082-C by Rust

---
+++ arc/082/c.rs
@@ -0,0 +1,55 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(usize);
+    let a = get!(Vec<usize>);
+
+    // a[i] = j を満たすiの個数を集計
+    // 集計結果を用いて解を求める
+    let mut dp = vec!(0; a.iter().max().unwrap() + 3);
+    for x in a {
+        dp[x] += 1;
+        dp[x + 1] += 1;
+        dp[x + 2] += 1;
+    }
+    let ans = dp.iter().max().unwrap();
+
+    println!("{}", ans);
+}
