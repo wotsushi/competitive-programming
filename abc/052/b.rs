530a80dce27186059bc3dce2656d4e783ddaf5f7
wotsushi <wotsushi@gmail.com>
2019-07-04 23:55:30 +0900

    Solve ABC052-B by Rust

---
+++ abc/052/b.rs
@@ -0,0 +1,57 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+use std::cmp;
+
+fn main() {
+    let N = get!(usize);
+    let S = get!(String);
+
+    let (ans, _) = S.chars().fold(
+        (0, 0),
+        |(m, acc), c| if c == 'I' {
+            let x = acc + 1;
+            (cmp::max(m, x), x)
+        } else {
+            (m, acc - 1)
+        }
+    );
+
+    println!("{}", ans);
+}
