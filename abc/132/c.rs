7bc74b1d0d3d67039a111c6a3878b8e5b4525e43
wotsushi <wotsushi@gmail.com>
2019-07-07 13:11:46 +0900

    Solve ABC132-C by Rust

---
+++ abc/132/c.rs
@@ -0,0 +1,49 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(usize);
+    let mut d = get!(Vec<usize>);
+
+    // dの右中央値と左中央値の差が解
+    d.sort();
+    let ans = d[N / 2] - d[N / 2 - 1];
+
+    println!("{}", ans);
+}
