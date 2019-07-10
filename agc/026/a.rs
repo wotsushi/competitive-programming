17b89c01eb1a212d71421a36fc8729eaf84f53c6
wotsushi <wotsushi@gmail.com>
2019-07-10 20:40:18 +0900

    Solve AGC026-A by Rust

---
+++ agc/026/a.rs
@@ -0,0 +1,56 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(usize);
+    let mut a = get!(Vec<usize>);
+
+    // 解はfloor(連続する同色のスライムの数 / 2) の和
+    a.push(0);
+    let (ans, _) = (0..N).fold(
+        (0, 1),
+        |(acc, b), i| if a[i] == a[i + 1] {
+            (acc, b + 1)
+        } else {
+            (acc + b / 2, 1)
+        }
+    );
+
+    println!("{}", ans);
+}
