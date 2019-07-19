a53aa25ec0af1aff0f9546262239eb13fce519d6
wotsushi <wotsushi@gmail.com>
2019-07-19 20:45:40 +0900

    Solve CADDi2018-A by Rust

---
+++ etc/caddi2018/a.rs
@@ -0,0 +1,55 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(usize);
+
+    let ans = N.to_string()
+        .chars()
+        .map(
+            |d| if d == '2' {
+                1
+            } else {
+                0
+            }
+        )
+        .sum::<usize>();
+
+    println!("{}", ans);
+}
