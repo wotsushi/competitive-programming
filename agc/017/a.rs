bfa52c5f6a501d9040018a73a2f7c20cc2d35866
wotsushi <wotsushi@gmail.com>
2019-07-24 22:22:22 +0900

    Solve AGC017-A by Rust

---
+++ agc/017/a.rs
@@ -0,0 +1,53 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (N, P) = get!(usize, i64);
+    let A = get!(Vec<usize>);
+
+    // Aがすべて偶数ならば (1-P) 2^N
+    // そうでなければ 2^{N-1}
+    let ans = if A.iter().all(|a| a % 2 == 0) {
+        (1 - P) * 2_i64.pow(N as u32)
+    } else {
+        2_i64.pow((N - 1) as u32)
+    };
+
+    println!("{}", ans);
+}
