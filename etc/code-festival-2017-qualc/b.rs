0b2772812f78b8027e7deec129b59799ecd451e8
wotsushi <wotsushi@gmail.com>
2019-07-27 00:27:15 +0900

    Solve code-festival-2017-qualc-B by Rust

---
+++ etc/code-festival-2017-qualc/b.rs
@@ -0,0 +1,51 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(u32);
+    let A = get!(Vec<i64>);
+
+    // 解は3^N - 2^(Aに含まれる偶数の個数)
+    let M = A.iter()
+        .filter(|&a| a % 2 == 0)
+        .count();
+    let ans = 3_i64.pow(N) - 2_i64.pow(M as u32);
+
+    println!("{}", ans);
+}
