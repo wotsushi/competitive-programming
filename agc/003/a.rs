70147f0f34c966323f16de289ea2d5b37bfa8ce4
wotsushi <wotsushi@gmail.com>
2019-07-31 21:52:16 +0900

    Solve AGC003-A by Rust

---
+++ agc/003/a.rs
@@ -0,0 +1,51 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let S = get!(String).chars().collect::<Vec<_>>();
+
+    // NとSが両方とも含まれる／含まれない、かつ、WとEが両方とも含まれる／含まれない ならば Yes
+    let ans = if !(S.contains(&'N') ^ S.contains(&'S')) && !(S.contains(&'W') ^ S.contains(&'E')) {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
+}
