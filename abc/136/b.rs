3cebd926a48719738ecd523351ce360ad322c3d9
wotsushi <wotsushi@gmail.com>
2019-08-05 20:12:17 +0900

    Solve ABC136-B by Rust

---
+++ abc/136/b.rs
@@ -0,0 +1,51 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+use std::cmp;
+
+fn main() {
+    let N = get!(i64);
+
+    // 1桁, 3桁, 5桁の個数を数え上げる
+    let ans = cmp::min(9, N) +
+        cmp::max(0, cmp::min(999, N) - 99) +
+        cmp::max(0, cmp::min(99999, N) - 9999);
+
+    println!("{}", ans);
+}
