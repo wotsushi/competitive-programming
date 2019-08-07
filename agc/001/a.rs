95770bf61c236538db143ba09826eb58461a0289
wotsushi <wotsushi@gmail.com>
2019-08-07 21:45:03 +0900

    Solve AGC001-A by Rust

---
+++ agc/001/a.rs
@@ -0,0 +1,51 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+use std::cmp;
+
+fn main() {
+    let N = get!(usize);
+    let mut L = get!(Vec<usize>);
+
+    L.sort();
+    // 貪欲に、小さい串同士を組み合わせて使えばよい
+    let ans = (0..N).map(|i| cmp::min(L[2 * i], L[2 * i + 1])).sum::<usize>();
+
+    println!("{}", ans);
+}
