815e376c1ff17eb678401c241aaa7baae7f36da8
wotsushi <wotsushi@gmail.com>
2019-08-08 20:15:05 +0900

    Solve aising2019-B by Rust

---
+++ etc/aising2019/b.rs
@@ -0,0 +1,52 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(usize);
+    let (A, B) = get!(usize, usize);
+    let P = get!(Vec<usize>);
+
+    let ans = vec![
+        P.iter().filter(|&p| p <= &A).count(),
+        P.iter().filter(|&p| &A < p && p <= &B).count(),
+        P.iter().filter(|&p| p > &B).count()
+    ].into_iter().min().unwrap();
+
+    println!("{}", ans);
+}
