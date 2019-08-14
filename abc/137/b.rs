d43e4c7689f1814bf0bb4736342de4ddbc3de9a5
wotsushi <wotsushi@gmail.com>
2019-08-14 09:28:13 +0900

    Solve ABC137-B by Rust

---
+++ abc/137/b.rs
@@ -0,0 +1,50 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (K, X) = get!(i64, i64);
+
+    // 座標X - K + 1 から X + K - 1 まで石が置かれる
+    let ans = ((X - K + 1)..(X + K)).into_iter()
+        .map(|i| i.to_string())
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    println!("{}", ans);
+}
