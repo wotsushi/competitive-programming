de148d4c2fcd18898240184664582776c76c6ee0
wotsushi <wotsushi@gmail.com>
2019-08-18 10:57:54 +0900

    Solve AGC037-A by Rust

---
+++ agc/037/a.rs
@@ -0,0 +1,58 @@
+macro_rules! get {
+    (Vec<$t:ty>) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+}
+
+
+fn main() {
+    let S = get!(String);
+
+    // 先頭から貪欲に分割する
+    let (ans, _, _) = S.chars().fold(
+        (0, "".to_string(), "".to_string()),
+        |(res, t, prev), c| {
+            let x = format!("{}{}", t, c);
+            if x == prev {
+                (res, x, prev)
+            } else {
+                (res + 1, "".to_string(), x)
+            }
+        }
+    );
+
+    println!("{}", ans);
+}
