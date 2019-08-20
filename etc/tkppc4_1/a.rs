e5336b0c25b9972c966a41811d495420d607a1ea
wotsushi <wotsushi@gmail.com>
2019-08-20 23:46:50 +0900

    Solve tkppc4_1-A by Rust

---
+++ etc/tkppc4_1/a.rs
@@ -0,0 +1,17 @@
+#![allow(non_snake_case)]
+
+fn main() {
+    let (N, W): (i64, i64) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+
+    let ans = N - 1;
+
+    println!("{}", ans);
+}
