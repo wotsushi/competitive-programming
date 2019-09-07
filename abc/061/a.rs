1f4066e861f3e125d4b3772d2d86258f01e3e69b
wotsushi <wotsushi@gmail.com>
2019-09-07 19:19:16 +0900

    Solve ABC061-A by Rust

---
+++ abc/061/a.rs
@@ -0,0 +1,23 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+
+fn main() {
+    let (A, B, C): (i64, i64, i64) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+
+    let ans = if A <= C && C <= B {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
+}
