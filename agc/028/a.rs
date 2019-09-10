847c17176ceb8b4ce0ab698d0d9055bc0f0bc463
wotsushi <wotsushi@gmail.com>
2019-09-10 21:25:43 +0900

    Solve AGC028-A by Rust

---
+++ agc/028/a.rs
@@ -0,0 +1,44 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let (N, M): (usize, usize) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+    let S: Vec<char> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().chars().collect()
+    };
+    let T: Vec<char> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().chars().collect()
+    };
+
+    fn gcd(a: usize, b: usize) -> usize {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+
+    // よい文字列を作れるならば|X| = lcm(N, M) である
+    let G = gcd(N, M);
+    let ans = if (0..G).all(|i| S[i * N / G] == T[i * M / G]) {
+        (N * M / G) as i64
+    } else {
+        -1
+    };
+
+    println!("{}", ans);
+}
+
