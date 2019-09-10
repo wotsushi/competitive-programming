c8074fd1fe254fbde255b21a58ad2f4d7505eb9b
wotsushi <wotsushi@gmail.com>
2019-09-10 22:03:39 +0900

    Refactoring

--- abc/062/b.rs
+++ abc/062/b.rs
@@ -1,60 +1,42 @@
-macro_rules! get {
-      ($t:ty) => {
-          {
-              let mut line: String = String::new();
-              std::io::stdin().read_line(&mut line).unwrap();
-              line.trim().parse::<$t>().unwrap()
-          }
-      };
-      ($($t:ty),*) => {
-          {
-              let mut line: String = String::new();
-              std::io::stdin().read_line(&mut line).unwrap();
-              let mut iter = line.split_whitespace();
-              (
-                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
-              )
-          }
-      };
-      ($t:ty; $n:expr) => {
-          (0..$n).map(|_|
-              get!($t)
-          ).collect::<Vec<_>>()
-      };
-      ($($t:ty),*; $n:expr) => {
-          (0..$n).map(|_|
-              get!($($t),*)
-          ).collect::<Vec<_>>()
-      };
-      ($t:ty ;;) => {
-          {
-              let mut line: String = String::new();
-              std::io::stdin().read_line(&mut line).unwrap();
-              line.split_whitespace()
-                  .map(|t| t.parse::<$t>().unwrap())
-                  .collect::<Vec<_>>()
-          }
-      };
-      ($t:ty ;; $n:expr) => {
-          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
-      };
-}
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
 fn main() {
-    // 入力
-    let (H, W) = get!(i64, i64);
-    let a = get!(String; H);
+    let (H, W): (usize, usize) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+    let a: Vec<Vec<char>> = {
+        let mut a = vec![];
+        for _ in 0..H {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            a.push(line.trim().chars().collect());
+        }
+        a
+    };
 
-    let x = (0..(W + 2)).map(|_| "#")
-        .collect::<Vec<_>>()
-        .join("");
-    let ans = [
-        &x[..],
-        &a.into_iter().map(|b| format!("#{}#", b))
+    let wall = (0..(W + 2)).map(|_| "#").collect::<Vec<_>>().join("");
+    let ans = format!(
+        "{}\n{}\n{}",
+        wall,
+        a.iter()
+            .map(|r| format!(
+                "#{}#",
+                r.iter()
+                    .map(std::string::ToString::to_string)
+                    .collect::<Vec<_>>().join("")
+            ))
             .collect::<Vec<_>>()
             .join("\n"),
-        &x[..]
-    ].join("\n");
-    // 出力
+        wall
+    );
+
     println!("{}", ans);
 }
