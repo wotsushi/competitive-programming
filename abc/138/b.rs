720f72dc580d808d2b0ce1be6ca0118aaafc154b
wotsushi <wotsushi@gmail.com>
2019-09-15 09:36:54 +0900

    Solve ABC138-B by Rust

---
+++ abc/138/b.rs
@@ -0,0 +1,22 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let N: usize = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().parse().unwrap()
+    };
+    let A: Vec<i64> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect()
+    };
+
+    let ans = 1.0 / A.iter().map(|&a| 1.0 / (a as f64)).sum::<f64>();
+
+    println!("{}", ans);
+}
