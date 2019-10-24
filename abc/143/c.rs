263b4fc32dfa93b7d0c02fb7e388b76377d628a1
wotsushi <wotsushi@gmail.com>
2019-10-24 22:28:03 +0900

    Solve ABC143-C by Rust

---
+++ abc/143/c.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let N: usize = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().parse().unwrap()
+    };
+    let S: Vec<char> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().chars().collect()
+    };
+
+    let ans = (1..N).filter(|&i| S[i - 1] != S[i]).count() + 1;
+
+    println!("{}", ans);
+}
