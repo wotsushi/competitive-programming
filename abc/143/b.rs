3363d93544123a6bf639ca4b963284102647196c
wotsushi <wotsushi@gmail.com>
2019-10-25 22:40:22 +0900

    Solve ABC143-B by Rust

---
+++ abc/143/b.rs
@@ -0,0 +1,23 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let N: usize = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().parse().unwrap()
+    };
+    let d: Vec<i64> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect()
+    };
+
+    let s = d.iter().sum::<i64>();
+    let ans = d.iter().map(|&x| x * (s - x)).sum::<i64>() / 2;
+
+    println!("{}", ans);
+}
