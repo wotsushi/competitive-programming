f6ea5b9c27261fd809bee5b982a79d3a6b879665
wotsushi <wotsushi@gmail.com>
2019-10-29 12:38:51 +0900

    Solve ARC088-C by Rust

---
+++ arc/088/c.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let (X, Y): (i64, i64) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+
+    let ans = format!("{:b}", Y / X).len();
+
+    println!("{}", ans);
+}
