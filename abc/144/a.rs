e1dfbba564ca4d4b7b1e4e9b479224003cee7f60
wotsushi <wotsushi@gmail.com>
2019-11-01 12:47:46 +0900

    Solve ABC144-A by Rust

---
+++ abc/144/a.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let (A, B): (i64, i64) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+
+    let ans = if A <= 9 && B <= 9 { A * B } else { - 1 };
+
+    println!("{}", ans);
+}
