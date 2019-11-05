119ab0f9bdb86ecf8a9876db177ffc7b5a802840
wotsushi <wotsushi@gmail.com>
2019-11-05 12:37:48 +0900

    Solve code-festival-2016-quala-B by Rust

---
+++ etc/code-festival-2016-quala/b.rs
@@ -0,0 +1,22 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let N: usize = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().parse().unwrap()
+    };
+    let a: Vec<usize> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect()
+    };
+
+    let ans = (0..N).filter(|&i| a[a[i] - 1] == i + 1).count() / 2;
+
+    println!("{}", ans);
+}
