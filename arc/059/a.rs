4868cc6f9c993907824615561c2a657eaf9680ea
wotsushi <wotsushi@gmail.com>
2019-12-10 21:57:21 +0900

    Solve ARC059-A by Rust

---
+++ arc/059/a.rs
@@ -0,0 +1,15 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let N: i64 = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().parse().unwrap()
+    };
+
+    let ans = N * (N + 1) / 2;
+
+    println!("{}", ans);
+}
