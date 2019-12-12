308853013c3441eb19f069e45cd47023c24fed9e
wotsushi <wotsushi@gmail.com>
2019-12-12 20:00:43 +0900

    Solve yahoo-procon2019-qual-F

---
+++ etc/yahoo-procon2019-qual/f.rs
@@ -0,0 +1,152 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let S: Vec<char> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().chars().collect()
+    };
+
+    const MOD: i64 = 998244353;
+    fn modinv(x: i64) -> i64 {
+        let mut a = x;
+        let mut b = MOD;
+        let mut u = 1;
+        let mut v = 0;
+        while b != 0 {
+            let t = a / b;
+            a -= t * b;
+            std::mem::swap(&mut a, &mut b);
+            u -= t * v;
+            std::mem::swap(&mut u, &mut v);
+        }
+        u %= MOD;
+        if u >= 0 {
+            u
+        } else {
+            u + MOD
+        }
+    }
+    #[derive(Copy, Clone)]
+    struct ModInt {
+        x: i64,
+    }
+    impl ModInt {
+        fn new(x: i64) -> ModInt {
+            let y = if x >= 0 {
+                x % MOD
+            } else {
+                (x + (1 - x / MOD) * MOD) % MOD
+            };
+            ModInt { x: y }
+        }
+    }
+    impl std::ops::Add<ModInt> for ModInt {
+        type Output = ModInt;
+        fn add(self, other: ModInt) -> ModInt {
+            ModInt::new(self.x + other.x)
+        }
+    }
+    impl std::ops::Add<i64> for ModInt {
+        type Output = ModInt;
+        fn add(self, other: i64) -> ModInt {
+            ModInt::new(self.x + other)
+        }
+    }
+    impl std::ops::Add<usize> for ModInt {
+        type Output = ModInt;
+        fn add(self, other: usize) -> ModInt {
+            ModInt::new(self.x + (other as i64))
+        }
+    }
+    impl std::ops::Sub<ModInt> for ModInt {
+        type Output = ModInt;
+        fn sub(self, other: ModInt) -> ModInt {
+            ModInt::new(self.x - other.x)
+        }
+    }
+    impl std::ops::Sub<i64> for ModInt {
+        type Output = ModInt;
+        fn sub(self, other: i64) -> ModInt {
+            ModInt::new(self.x - other)
+        }
+    }
+    impl std::ops::Sub<usize> for ModInt {
+        type Output = ModInt;
+        fn sub(self, other: usize) -> ModInt {
+            ModInt::new(self.x - (other as i64))
+        }
+    }
+    impl std::ops::Mul<ModInt> for ModInt {
+        type Output = ModInt;
+        fn mul(self, other: ModInt) -> ModInt {
+            ModInt::new(self.x * other.x)
+        }
+    }
+    impl std::ops::Mul<i64> for ModInt {
+        type Output = ModInt;
+        fn mul(self, other: i64) -> ModInt {
+            ModInt::new(self.x * other)
+        }
+    }
+    impl std::ops::Mul<usize> for ModInt {
+        type Output = ModInt;
+        fn mul(self, other: usize) -> ModInt {
+            ModInt::new(self.x * (other as i64))
+        }
+    }
+    impl std::ops::Div<ModInt> for ModInt {
+        type Output = ModInt;
+        fn div(self, other: ModInt) -> ModInt {
+            ModInt::new(self.x * modinv(other.x as i64))
+        }
+    }
+    impl std::ops::Div<i64> for ModInt {
+        type Output = ModInt;
+        fn div(self, other: i64) -> ModInt {
+            ModInt::new(self.x * modinv(other))
+        }
+    }
+    impl std::ops::Div<usize> for ModInt {
+        type Output = ModInt;
+        fn div(self, other: usize) -> ModInt {
+            ModInt::new(self.x * modinv(other as i64))
+        }
+    }
+    impl std::fmt::Display for ModInt {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(f, "{}", self.x)
+        }
+    }
+
+    let N = S.len();
+    let mut dp = vec![vec![ModInt::new(0); N + 2]; 2 * N + 1];
+    dp[0][0] = ModInt::new(1);
+    for i in 1..(N + 1) {
+        let s = S[i - 1].to_digit(10).unwrap() as usize;
+        for j in 0..N {
+            let k = j + s;
+            if k > 0 {
+                dp[i][k - 1] = dp[i][k - 1] + dp[i - 1][j];
+            }
+            if i + 1 > k {
+                dp[i][k] = dp[i][k] + dp[i - 1][j];
+            }
+        }
+    }
+    for i in (N + 1)..(2 * N + 1) {
+        for j in 0..(N + 2) {
+            if j > 0 {
+                dp[i][j - 1] = dp[i][j - 1] + dp[i - 1][j];
+            }
+            if 2 * N - i + 1 > j {
+                dp[i][j] = dp[i][j] + dp[i - 1][j];
+            }
+        }
+    }
+    let ans = dp[2 * N][0];
+
+    println!("{}", ans);
+}
