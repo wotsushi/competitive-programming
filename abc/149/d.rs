4cec78aebb5e63fd6c5c04a3eae01ba603a4b086
wotsushi <wotsushi@gmail.com>
2020-01-11 09:29:38 +0900

    Solve ABC149-D by Rust

---
+++ abc/149/d.rs
@@ -0,0 +1,46 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    let (N, K): (usize, usize) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+        )
+    };
+    let (R, S, P): (i64, i64, i64) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+        )
+    };
+    let T: Vec<char> = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().chars().collect()
+    };
+
+    let mut free = vec![true; N];
+    let mut res = 0;
+    for i in 0..N {
+        if i < K || free[i - K] || T[i] != T[i - K] {
+            res += match T[i] {
+                'r' => P,
+                's' => R,
+                _ => S,
+            };
+            free[i] = false;
+        }
+    }
+    let ans = res;
+
+    println!("{}", ans);
+}
