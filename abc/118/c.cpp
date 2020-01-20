a1221319b64b2cca0eb46d851af33eca5eaa07a7
wotsushi <wotsushi@gmail.com>
2020-01-20 21:28:48 +0900

    Solve ABC118-C by C++

---
+++ abc/118/c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+ll N;
+vi A;
+
+int main()
+{
+    cin >> N;
+    A = vi(N);
+    REP(i, N)
+    {
+        cin >> A[i];
+    }
+
+    ll ans = 0;
+    REP(i, N)
+    {
+        ans = gcd(ans, A[i]);
+    }
+
+    OUT(ans);
+}
