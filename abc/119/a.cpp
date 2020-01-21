90722f1e596506ee0775916ac8fb94167a3a10af
wotsushi <wotsushi@gmail.com>
2020-01-21 21:24:17 +0900

    Solve ABC119-A by C++

---
+++ abc/119/a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+string S;
+
+int main()
+{
+    cin >> S;
+
+    string ans;
+    if (S <= "2019/04/30")
+    {
+        ans = "Heisei";
+    }
+    else
+    {
+        ans = "TBD";
+    }
+
+    OUT(ans);
+}
