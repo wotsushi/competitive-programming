e39fa9fd5d1168b4a553e19e878227047ee601ba
wotsushi <wotsushi@gmail.com>
2020-01-29 17:19:46 +0900

    Solve ABC007-A by C++

---
+++ abc/007/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+ll n;
+
+int main()
+{
+    cin >> n;
+
+    ll ans = n - 1;
+
+    OUT(ans);
+}
