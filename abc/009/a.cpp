e13cb06c9a92da518511760a584705f46c3b40a8
wotsushi <wotsushi@gmail.com>
2020-01-31 14:15:43 +0900

    Solve ABC009-A by C++

---
+++ abc/009/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+
+ll N;
+
+int main()
+{
+    cin >> N;
+
+    ll ans = CEIL(N, 2);
+
+    OUT(ans);
+}
