b16e576c5d84a12ff11e832c1f879761bb30ebc5
wotsushi <wotsushi@gmail.com>
2020-02-04 21:45:54 +0900

    Solve ABC011-B by C++

---
+++ abc/011/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+string S;
+
+int main()
+{
+    cin >> S;
+
+    string ans = S;
+    ans[0] = toupper(ans[0]);
+    for (ll i = 1; i < S.size(); ++i)
+    {
+        ans[i] = tolower(ans[i]);
+    }
+
+    OUT(ans);
+}
