71d6cb988b9bb2cbd31e1d55e1a3ef8d5824d04e
wotsushi <wotsushi@gmail.com>
2020-02-06 22:49:44 +0900

    Solve ABC012-B by C++

---
+++ abc/012/b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+ll N;
+
+int main()
+{
+    cin >> N;
+
+    ll h = N / 3600;
+    ll m = (N % 3600) / 60;
+    ll s = N % 60;
+    cout << setw(2) << setfill('0') << h << ":" << setw(2) << setfill('0') << m << ":" << setw(2) << setfill('0') << s << endl;
+}
