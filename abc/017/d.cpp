3ae119347c5e551fb734bd333e46de3635f9660c
wotsushi <wotsushi@gmail.com>
2020-02-21 23:01:38 +0900

    Solve ABC017-D by C++

---
+++ abc/017/d.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+ll N, M;
+vi f;
+
+class mint
+{
+public:
+    ll a;
+
+    mint(const ll x = 0) : a(x % MOD) {}
+    ll &value() { return a; }
+    mint pow(ll rhs)
+    {
+        ll exp = rhs;
+        mint res = mint(1);
+        mint p = mint(a);
+        while (exp)
+        {
+            if (exp & 1)
+            {
+                res *= p;
+            }
+            exp >>= 1;
+            p *= p;
+        }
+        return res;
+    }
+    mint pow(mint rhs)
+    {
+        return pow(rhs.a);
+    }
+    mint &operator+=(ll rhs)
+    {
+        a += rhs;
+        if (a >= MOD)
+        {
+            a -= MOD;
+        }
+        return *this;
+    }
+    mint &operator+=(mint rhs)
+    {
+        *this += rhs.a;
+        return *this;
+    }
+    mint &operator-=(ll rhs)
+    {
+        if (a < rhs)
+        {
+            a += MOD;
+        }
+        a -= rhs;
+        return *this;
+    }
+    mint &operator-=(mint rhs)
+    {
+        *this -= rhs.a;
+        return *this;
+    }
+    mint &operator*=(ll rhs)
+    {
+        a = a * rhs % MOD;
+        return *this;
+    }
+    mint &operator*=(mint rhs)
+    {
+        *this *= rhs.a;
+        return *this;
+    }
+    mint &operator/=(ll rhs)
+    {
+        *this *= mint(rhs).pow(MOD - 2);
+        return *this;
+    }
+    mint &operator/=(mint rhs)
+    {
+        *this /= rhs.a;
+        return *this;
+    }
+};
+
+mint operator+(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) += rhs;
+}
+mint operator+(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) += rhs;
+}
+mint operator+(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) += rhs;
+}
+mint operator-(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) -= rhs;
+}
+mint operator-(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) -= rhs;
+}
+mint operator-(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) -= rhs;
+}
+mint operator*(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) *= rhs;
+}
+mint operator*(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) *= rhs;
+}
+mint operator*(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) *= rhs;
+}
+mint operator/(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) /= rhs;
+}
+mint operator/(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) /= rhs;
+}
+mint operator/(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) /= rhs;
+}
+
+int main()
+{
+    cin >> N >> M;
+    f = vi(N);
+    REP(i, N)
+    {
+        cin >> f[i];
+    }
+
+    reverse(ALL(f));
+    vector<mint> acc = vector<mint>(N + 2);
+    vi p = vi(M + 1);
+    acc[0] = mint(0);
+    acc[1] = mint(1);
+    ll j = 0;
+    mint res;
+    for (ll i = 2; i <= N + 1; ++i)
+    {
+        j = max(j, p[f[i - 2]]);
+        res = acc[i - 1] - acc[j];
+        acc[i] = res + acc[i - 1];
+        p[f[i - 2]] = i - 1;
+    }
+    ll ans = res.a;
+
+    OUT(ans);
+}
