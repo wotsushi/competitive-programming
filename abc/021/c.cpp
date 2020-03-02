b3d94c8744328c2dfa3fad126c6fea2e2189fe20
wotsushi <wotsushi@gmail.com>
2020-03-02 22:45:00 +0900

    Solve ABC021-C by C++

---
+++ abc/021/c.cpp
@@ -0,0 +1,189 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a, n) \
+    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+class mint
+{
+public:
+    ll a;
+
+    mint(const ll x = 0) : a(x % MOD) {}
+    ll &value() { return a; }
+    mint pow(ll rhs)
+    {
+        ll exp = rhs;
+        mint res = mint(1);
+        mint p = mint(a);
+        while (exp)
+        {
+            if (exp & 1)
+            {
+                res *= p;
+            }
+            exp >>= 1;
+            p *= p;
+        }
+        return res;
+    }
+    mint pow(mint rhs)
+    {
+        return pow(rhs.a);
+    }
+    mint &operator+=(ll rhs)
+    {
+        a += rhs;
+        if (a >= MOD)
+        {
+            a -= MOD;
+        }
+        return *this;
+    }
+    mint &operator+=(mint rhs)
+    {
+        *this += rhs.a;
+        return *this;
+    }
+    mint &operator-=(ll rhs)
+    {
+        if (a < rhs)
+        {
+            a += MOD;
+        }
+        a -= rhs;
+        return *this;
+    }
+    mint &operator-=(mint rhs)
+    {
+        *this -= rhs.a;
+        return *this;
+    }
+    mint &operator*=(ll rhs)
+    {
+        a = a * rhs % MOD;
+        return *this;
+    }
+    mint &operator*=(mint rhs)
+    {
+        *this *= rhs.a;
+        return *this;
+    }
+    mint &operator/=(ll rhs)
+    {
+        *this *= mint(rhs).pow(MOD - 2);
+        return *this;
+    }
+    mint &operator/=(mint rhs)
+    {
+        *this /= rhs.a;
+        return *this;
+    }
+};
+
+mint operator+(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) += rhs;
+}
+mint operator+(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) += rhs;
+}
+mint operator+(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) += rhs;
+}
+mint operator-(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) -= rhs;
+}
+mint operator-(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) -= rhs;
+}
+mint operator-(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) -= rhs;
+}
+mint operator*(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) *= rhs;
+}
+mint operator*(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) *= rhs;
+}
+mint operator*(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) *= rhs;
+}
+mint operator/(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) /= rhs;
+}
+mint operator/(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) /= rhs;
+}
+mint operator/(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) /= rhs;
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    ll a, b;
+    cin >> a >> b;
+    ll M;
+    cin >> M;
+    vi x(M), y(M);
+    REP(i, M)
+    {
+        cin >> x[i] >> y[i];
+    }
+
+    auto G = vector<vector<bool>>(N + 1, vector<bool>(N + 1));
+    REP(i, M)
+    {
+        G[x[i]][y[i]] = G[y[i]][x[i]] = true;
+    }
+    vector<mint> dp(N + 1);
+    vi d(N + 1, INF);
+    queue<ll> q;
+    dp[a] = mint(1);
+    d[a] = 0;
+    q.push(a);
+    while (!q.empty())
+    {
+        ll i = q.front();
+        q.pop();
+        for (ll j = 1; j <= N; ++j)
+        {
+            if (G[i][j] and d[i] + 1 <= d[j])
+            {
+                dp[j] += dp[i];
+                if (d[i] + 1 < d[j])
+                {
+                    d[j] = d[i] + 1;
+                    q.push(j);
+                }
+            }
+        }
+    }
+    ll ans = dp[b].a;
+
+    OUT(ans);
+}
