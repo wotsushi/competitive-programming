01d93abd5bdfe90e6274350164364f58165e06f0
wotsushi <wotsushi@gmail.com>
2020-04-03 19:20:03 +0900

    Solve dp-M by C++

---
+++ etc/dp/m.cpp
@@ -0,0 +1,207 @@
+#pragma region template 1.0
+#pragma region def
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REP1(i, n) for (ll i = 1; i <= (n); ++i)
+#define OUT(x) cout << (x) << endl;
+#define OUTA(a) \
+    REP(i, (a).size()) { cout << (a[i]) << (i == (a).size() - 1 ? "\n" : " "); }
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#pragma endregion enddef
+
+#pragma region mint 1.0
+struct mint
+{
+    ll a;
+
+    mint(ll x = 0) : a((x % MOD + MOD) % MOD) {}
+    mint pow(ll rhs)
+    {
+        ll exp = rhs;
+        mint res = mint(1);
+        mint p = mint(a);
+        while (exp)
+        {
+            if (exp & 1)
+            {
+                res *= p;
+            }
+            exp >>= 1;
+            p *= p;
+        }
+        return res;
+    }
+    mint pow(mint rhs)
+    {
+        return pow(rhs.a);
+    }
+    mint &operator+=(ll rhs)
+    {
+        a += rhs;
+        if (a >= MOD)
+        {
+            a -= MOD;
+        }
+        return *this;
+    }
+    mint &operator+=(mint rhs)
+    {
+        *this += rhs.a;
+        return *this;
+    }
+    mint &operator-=(ll rhs)
+    {
+        if (a < rhs)
+        {
+            a += MOD;
+        }
+        a -= rhs;
+        return *this;
+    }
+    mint &operator-=(mint rhs)
+    {
+        *this -= rhs.a;
+        return *this;
+    }
+    mint &operator*=(ll rhs)
+    {
+        if (rhs < 0)
+        {
+            rhs += MOD;
+        }
+        a = a * rhs % MOD;
+        return *this;
+    }
+    mint &operator*=(mint rhs)
+    {
+        *this *= rhs.a;
+        return *this;
+    }
+    mint &operator/=(ll rhs)
+    {
+        *this *= mint(rhs).pow(MOD - 2);
+        return *this;
+    }
+    mint &operator/=(const mint rhs)
+    {
+        *this /= rhs.a;
+        return *this;
+    }
+    mint &operator++()
+    {
+        *this += 1;
+        return *this;
+    }
+    mint &operator++(int)
+    {
+        *this += 1;
+        return *this;
+    }
+    mint &operator--()
+    {
+        *this -= 1;
+        return *this;
+    }
+    mint &operator--(int)
+    {
+        *this -= 1;
+        return *this;
+    }
+};
+
+mint operator+(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) += rhs;
+}
+mint operator+(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) += rhs;
+}
+mint operator+(const ll &lhs, mint &rhs)
+{
+    return mint(lhs) += rhs;
+}
+mint operator-(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) -= rhs;
+}
+mint operator-(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) -= rhs;
+}
+mint operator-(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) -= rhs;
+}
+mint operator*(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) *= rhs;
+}
+mint operator*(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) *= rhs;
+}
+mint operator*(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) *= rhs;
+}
+mint operator/(const mint &lhs, const mint &rhs)
+{
+    return mint(lhs.a) /= rhs;
+}
+mint operator/(const mint &lhs, const ll &rhs)
+{
+    return mint(lhs.a) /= rhs;
+}
+mint operator/(const ll &lhs, const mint &rhs)
+{
+    return mint(lhs) /= rhs;
+}
+istream &operator>>(istream &is, mint &i)
+{
+    is >> i.a;
+    return is;
+}
+ostream &operator<<(ostream &os, const mint &i)
+{
+    os << i.a;
+    return os;
+}
+#pragma endregion mint
+
+int main()
+{
+    ll N, K;
+    cin >> N >> K;
+    vi a(N);
+    REP(i, N)
+    {
+        cin >> a[i];
+    }
+
+    vector<vector<mint>> dp(N + 1, vector<mint>(K + 2, mint(1)));
+    dp[0][0] = mint(0);
+    REP(i, N)
+    {
+        dp[i + 1][0] = mint(0);
+        REP1(j, K + 1)
+        {
+            dp[i + 1][j] = dp[i + 1][j - 1] + dp[i][j] - dp[i][max(0LL, j - 1 - a[i])];
+        }
+    }
+    mint ans = dp[N][K + 1] - dp[N][K];
+
+    OUT(ans);
+}
+#pragma endregion template
