7e41ac39cfb1b5fe2c34bc42cd9579ff8aad60b4
wotsushi <wotsushi@gmail.com>
2020-04-22 19:04:11 +0900

    Solve keyence2020-B by C++

--- etc/keyence2020/b.cpp
+++ etc/keyence2020/b.cpp
@@ -1,52 +1,109 @@
+#pragma region template 2.4
 #include <bits/stdc++.h>
 using namespace std;
+template <typename T>
+using pq_asc = priority_queue<T, vector<T>, greater<T>>;
 typedef long long ll;
 typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<string> vs;
 #define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REP1(i, n) for (ll i = 1; i <= (n); ++i)
+#define FOR(i, a) for (auto &i : a)
+#define CH(f, x, y) x = f(x, y)
+#define IN(T, x) \
+    T x;         \
+    cin >> x;
+#define AIN(T, a, n) \
+    vector<T> a(n);  \
+    FOR(i, a)        \
+    cin >> i;
+#define A2IN(T1, a, T2, b, n) \
+    vector<T1> a(n);          \
+    vector<T2> b(n);          \
+    REP(i, n)                 \
+    cin >> a[i] >> b[i];
 #define OUT(x) cout << (x) << endl;
-#define OUTA(a, n) \
-    REP(i, n) { cout << (a[i]) << (i == n - 1 ? "\n" : " "); }
-#define SORT(a) sort(a.begin(), a.end())
+#define FOUT(x) cout << fixed << setprecision(15) << (x) << endl;
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
 #define RSORT(a) \
     SORT(a);     \
-    reverse(a.begin(), a.end())
+    reverse(ALL(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define DUMPA(a)          \
+    cout << #a << " = {"; \
+    JOUT(ALL(a), cout, ", ") << "}" << endl;
+
+template <typename T>
+ostream &JOUT(T s, T e, ostream &os = cout, string sep = " ")
+{
+    if (s != e)
+    {
+        os << *s;
+        ++s;
+    }
+    while (s != e)
+    {
+        os << sep << *s;
+        ++s;
+    }
+    return os;
+}
+
+ostream &YES(bool cond, string yes = "Yes", string no = "No", ostream &os = cout)
+{
+    if (cond)
+    {
+        os << yes << endl;
+    }
+    else
+    {
+        os << no << endl;
+    }
+    return os;
+}
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    JOUT(ALL(v), os, ", ") << ']';
+    return os;
+}
+
 const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
-
-typedef pair<ll, ll> ii;
-typedef vector<ii> vp;
-#define OUTA2(a, n) \
-    REP(i, n) { cout << (a[i]).first << " " << (a[i]).second << endl; }
-
-ll N;
-vi X, L;
+#pragma endregion template
 
 int main()
 {
-    cin >> N;
-    X = vi(N);
-    L = vi(N);
-    REP(i, N)
-    {
-        cin >> X[i] >> L[i];
-    }
-
-    vp A = vp(N);
+    IN(ll, N);
+    A2IN(ll, X, ll, L, N);
+    vii A(N);
     REP(i, N)
     {
         A[i] = ii(X[i] + L[i], X[i] - L[i]);
     }
     SORT(A);
-    ll prev = -INF;
     ll ans = 0;
+    ll r = -INF;
     REP(i, N)
     {
-        if (prev <= A[i].second)
+        if (r <= A[i].second)
         {
             ++ans;
-            prev = A[i].first;
+            r = A[i].first;
         }
     }
-
     OUT(ans);
 }
