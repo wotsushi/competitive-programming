ca9b09e79bc8d60737a277eead2f2559127f7a3b
wotsushi <wotsushi@gmail.com>
2020-04-23 21:12:26 +0900

    Solve past201912-H by C++

---
+++ etc/past201912/h.cpp
@@ -0,0 +1,147 @@
+#pragma region template 2.4
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+using pq_asc = priority_queue<T, vector<T>, greater<T>>;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REP1(i, n) for (ll i = 1; i <= (n); ++i)
+#define FOR(i, a) for (auto &i : a)
+#define CH(f, x, y) x = f(x, y)
+#define IN(T, x) \
+    T x;         \
+    cin >> x;
+#define AIN(T, a, n) \
+    vector<T> a(n);  \
+    FOR(i, a)        \
+    cin >> i;
+#define A2IN(T1, a, T2, b, n) \
+    vector<T1> a(n);          \
+    vector<T2> b(n);          \
+    REP(i, n)                 \
+    cin >> a[i] >> b[i];
+#define OUT(x) cout << (x) << endl;
+#define FOUT(x) cout << fixed << setprecision(15) << (x) << endl;
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define DUMPA(a)          \
+    cout << #a << " = {"; \
+    JOUT(ALL(a), cout, ", ") << "}" << endl;
+
+template <typename T>
+ostream &JOUT(T s, T e, ostream &os = cout, string sep = " ")
+{
+    if (s != e)
+    {
+        os << *s;
+        ++s;
+    }
+    while (s != e)
+    {
+        os << sep << *s;
+        ++s;
+    }
+    return os;
+}
+
+ostream &YES(bool cond, string yes = "Yes", string no = "No", ostream &os = cout)
+{
+    if (cond)
+    {
+        os << yes << endl;
+    }
+    else
+    {
+        os << no << endl;
+    }
+    return os;
+}
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    JOUT(ALL(v), os, ", ") << ']';
+    return os;
+}
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#pragma endregion template
+
+int main()
+{
+    IN(ll, N);
+    AIN(ll, C, N);
+    IN(ll, Q);
+    ll m_odd = INF;
+    for (ll i = 0; i < N; i += 2)
+    {
+        CH(min, m_odd, C[i]);
+    }
+    ll m_even = INF;
+    for (ll i = 1; i < N; i += 2)
+    {
+        CH(min, m_even, C[i]);
+    }
+    ll a_odd = 0;
+    ll a_even = 0;
+    ll ans = 0;
+    REP(i, Q)
+    {
+        IN(ll, S);
+        if (S == 1)
+        {
+            IN(ll, x);
+            IN(ll, a);
+            if (x & 1 and C[x - 1] - a_odd >= a)
+            {
+                C[x - 1] -= a;
+                CH(min, m_odd, C[x - 1]);
+                ans += a;
+            }
+            if ((x & 1) == 0 and C[x - 1] - a_even >= a)
+            {
+                C[x - 1] -= a;
+                CH(min, m_even, C[x - 1]);
+                ans += a;
+            }
+        }
+        else if (S == 2)
+        {
+            IN(ll, a);
+            if (m_odd - a_odd >= a)
+            {
+                a_odd += a;
+                ans += (N + 1) / 2 * a;
+            }
+        }
+        else
+        {
+            IN(ll, a);
+            if (m_odd - a_odd >= a and m_even - a_even >= a)
+            {
+                a_odd += a;
+                a_even += a;
+                ans += N * a;
+            }
+        }
+    }
+    OUT(ans);
+}
