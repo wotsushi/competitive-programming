2e491849d09f06fbb9d501ab51a566a28f212d98
wotsushi <wotsushi@gmail.com>
2020-05-05 15:14:02 +0900

    Solve past202004-N by C++

---
+++ etc/past202004/n.cpp
@@ -0,0 +1,201 @@
+#pragma region template 2.4
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+using pq_asc = priority_queue<T, vector<T>, greater<T>>;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REP1(i, n) for (ll i = 1; i <= (n); ++i)
+#define FOR(i, a) for (auto &i : a)
+#define CH(f, x, y) x = f(x, y)
+#define IN(T, x) \
+    T x;         \
+    cin >> x;
+#define AIN(T, a, n) \
+    vector<T> a(n);  \
+    FOR(i, a)        \
+    cin >> i;
+#define A2IN(T1, a, T2, b, n) \
+    vector<T1> a(n);          \
+    vector<T2> b(n);          \
+    REP(i, n)                 \
+    cin >> a[i] >> b[i];
+#define OUT(x) cout << (x) << endl;
+#define FOUT(x) cout << fixed << setprecision(15) << (x) << endl;
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define DUMPA(a)          \
+    cout << #a << " = {"; \
+    JOUT(ALL(a), ", ", cout) << "}" << endl;
+
+template <typename T>
+ostream &JOUT(T s, T e, string sep = " ", ostream &os = cout)
+{
+    if (s != e)
+    {
+        os << *s;
+        ++s;
+    }
+    while (s != e)
+    {
+        os << sep << *s;
+        ++s;
+    }
+    return os;
+}
+
+ostream &YES(bool cond, string yes = "Yes", string no = "No", ostream &os = cout)
+{
+    if (cond)
+    {
+        os << yes << endl;
+    }
+    else
+    {
+        os << no << endl;
+    }
+    return os;
+}
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    JOUT(ALL(v), ", ", os) << ']';
+    return os;
+}
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#pragma endregion template
+#pragma region compress 0.1
+vi compress(vi &A)
+{
+    map<ll, ll> P;
+    FOR(a, A)
+    {
+        P.insert({a, 0});
+    }
+    ll p = 0;
+    for (auto [k, _] : P)
+    {
+        P[k] = p;
+        ++p;
+    }
+    vi B(A.size());
+    REP(i, A.size())
+    {
+        B[i] = P[A[i]];
+    }
+    return B;
+}
+#pragma endregion compress
+#pragma region bit 1.0
+template <typename T>
+struct bit
+{
+    ll n;
+    vector<T> a;
+
+    bit() {}
+
+    bit(ll n) : n(n)
+    {
+        a.assign(n + 1, 0);
+    }
+
+    void add(ll i, T x)
+    {
+        ++i;
+        while (i <= n)
+        {
+            a[i] += x;
+            i += i & -i;
+        }
+    }
+
+    T sum(ll i)
+    {
+        T s = 0;
+        while (i > 0)
+        {
+            s += a[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+};
+#pragma endregion bit
+
+int main()
+{
+    IN(ll, N);
+    IN(ll, Q);
+    vi xmin(N), ymin(N), D(N), C(N);
+    REP(i, N)
+    {
+        cin >> xmin[i] >> ymin[i] >> D[i] >> C[i];
+    }
+    A2IN(ll, A, ll, B, Q);
+    vi Y;
+    REP(i, N)
+    {
+        Y.push_back(ymin[i]);
+        Y.push_back(ymin[i] + D[i]);
+    }
+    REP(i, Q)
+    {
+        Y.push_back(B[i]);
+    }
+    vi Z = compress(Y);
+    using E = tuple<ll, ll, ll, ll>;
+    vector<E> e;
+    REP(i, N)
+    {
+        e.push_back({xmin[i], Z[2 * i], Z[2 * i + 1] + 1, C[i]});
+        e.push_back({xmin[i] + D[i] + 1, Z[2 * i], Z[2 * i + 1] + 1, -C[i]});
+    }
+    SORT(e);
+    using P = tuple<ll, ll, ll>;
+    vector<P> plans(Q);
+    REP(i, Q)
+    {
+        plans[i] = {A[i], Z[2 * N + i], i};
+    }
+    SORT(plans);
+    ll k = 0;
+    bit<ll> costs(Z.size() + 1);
+    vi ans(Q);
+    REP(i, Q)
+    {
+        auto [a, b, j] = plans[i];
+        while (k < 2 * N and get<0>(e[k]) <= a)
+        {
+            auto [_, y1, y2, c] = e[k];
+            costs.add(y1, c);
+            costs.add(y2, -c);
+            ++k;
+        }
+        ans[j] = costs.sum(b + 1);
+    }
+    FOR(a, ans)
+    {
+        OUT(a);
+    }
+}
