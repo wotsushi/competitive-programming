9b989d5f6316c37aebe5e7ef53e77ee975390ce6
wotsushi <wotsushi@gmail.com>
2020-05-11 21:48:45 +0900

    Solve ABC167-F by C++

---
+++ abc/167/f.cpp
@@ -0,0 +1,137 @@
+#pragma region template 2.4
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+using pq_asc = priority_queue<T, vector<T>, greater<T>>;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REP1(i, n) for (ll i = 1; i <= (n); ++i)
+#define FOR(i, a) for (auto &i : a)
+#define CH(f, x, y) x = f(x, y)
+#define IN(T, x) \
+    T x;         \
+    cin >> x;
+#define AIN(T, a, n) \
+    vector<T> a(n);  \
+    FOR(i, a)        \
+    cin >> i;
+#define A2IN(T1, a, T2, b, n) \
+    vector<T1> a(n);          \
+    vector<T2> b(n);          \
+    REP(i, n)                 \
+    cin >> a[i] >> b[i];
+#define OUT(x) cout << (x) << endl;
+#define FOUT(x) cout << fixed << setprecision(15) << (x) << endl;
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define DUMPA(a)          \
+    cout << #a << " = {"; \
+    JOUT(ALL(a), ", ", cout) << "}" << endl;
+
+template <typename T>
+ostream &JOUT(T s, T e, string sep = " ", ostream &os = cout)
+{
+    if (s != e)
+    {
+        os << *s;
+        ++s;
+    }
+    while (s != e)
+    {
+        os << sep << *s;
+        ++s;
+    }
+    return os;
+}
+
+ostream &YES(bool cond, string yes = "Yes", string no = "No", ostream &os = cout)
+{
+    if (cond)
+    {
+        os << yes << endl;
+    }
+    else
+    {
+        os << no << endl;
+    }
+    return os;
+}
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    JOUT(ALL(v), ", ", os) << ']';
+    return os;
+}
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#pragma endregion template
+
+using P = tuple<ll, ll, ll>;
+
+int main()
+{
+    IN(ll, N);
+    AIN(string, S, N);
+    vector<P> T;
+    REP(i, N)
+    {
+        ll d = 0;
+        ll m = 0;
+        FOR(c, S[i])
+        {
+            if (c == '(')
+            {
+                ++d;
+            }
+            else
+            {
+                --d;
+            }
+            if (d < 0)
+            {
+                CH(max, m, -d);
+            }
+        }
+        ll p;
+        if (d >= 0)
+        {
+            p = -INF;
+        }
+        else
+        {
+            p = -(m + d);
+        }
+        T.push_back({p, m, d});
+    }
+    SORT(T);
+    ll x = 0;
+    for (auto [_, m, d] : T)
+    {
+        if (x < m)
+        {
+            OUT("No");
+            return 0;
+        }
+        x += d;
+    }
+    YES(x == 0);
+}
