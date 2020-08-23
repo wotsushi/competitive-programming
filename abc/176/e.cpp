87fbb392c29f1e96d7402c482f0caf6ec33d0e04
wotsushi <wotsushi@gmail.com>
2020-08-23 13:40:29 +0900

    Solve ABC176-E by C++

---
+++ abc/176/e.cpp
@@ -0,0 +1,131 @@
+#pragma region template 2.4
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+using pq_asc = priority_queue<T, vector<T>, greater<T>>;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REP1(i, n) for (ll i = 1; i <= (n); ++i)
+#define FOR(i, a) for (auto &i : a)
+#define CH(f, x, y) x = f(x, y)
+#define IN(T, x) \
+    T x;         \
+    cin >> x;
+#define AIN(T, a, n) \
+    vector<T> a(n);  \
+    FOR(i, a)        \
+    cin >> i;
+#define A2IN(T1, a, T2, b, n) \
+    vector<T1> a(n);          \
+    vector<T2> b(n);          \
+    REP(i, n)                 \
+    cin >> a[i] >> b[i];
+#define OUT(x) cout << (x) << endl;
+#define FOUT(x) cout << fixed << setprecision(15) << (x) << endl;
+#define ALL(a) (a).begin(), (a).end()
+#define SORT(a) sort(ALL(a))
+#define RSORT(a) \
+    SORT(a);     \
+    reverse(ALL(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define DUMPA(a)          \
+    cout << #a << " = {"; \
+    JOUT(ALL(a), ", ", cout) << "}" << endl;
+
+template <typename T>
+ostream &JOUT(T s, T e, string sep = " ", ostream &os = cout)
+{
+    if (s != e)
+    {
+        os << *s;
+        ++s;
+    }
+    while (s != e)
+    {
+        os << sep << *s;
+        ++s;
+    }
+    return os;
+}
+
+ostream &YES(bool cond, string yes = "Yes", string no = "No", ostream &os = cout)
+{
+    if (cond)
+    {
+        os << yes << endl;
+    }
+    else
+    {
+        os << no << endl;
+    }
+    return os;
+}
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    JOUT(ALL(v), ", ", os) << ']';
+    return os;
+}
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+#pragma endregion template
+
+int main()
+{
+    IN(ll, H);
+    IN(ll, W);
+    IN(ll, M);
+    A2IN(ll, h, ll, w, M);
+    set<ii> S;
+    vi X(W + 1), Y(H + 1);
+    REP(i, M)
+    {
+        S.insert({h[i], w[i]});
+        ++Y[h[i]];
+        ++X[w[i]];
+    }
+    ll my = *max_element(ALL(Y));
+    ll mx = *max_element(ALL(X));
+    vi U, V;
+    REP1(i, H)
+    {
+        if (Y[i] == my)
+        {
+            V.push_back(i);
+        }
+    }
+    REP1(i, W)
+    {
+        if (X[i] == mx)
+        {
+            U.push_back(i);
+        }
+    }
+    FOR(u, U)
+    {
+        FOR(v, V)
+        {
+            if (S.count({v, u}) == 0)
+            {
+                OUT(mx + my);
+                return 0;
+            }
+        }
+    }
+    OUT(mx + my - 1);
+}
